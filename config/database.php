8861d356706df88b7b9f2bb149ca49796dbc2bc4
erenorren <regishashrn07@gmail.com>
2025-11-01 06:43:24 +0700

    update

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
 // /config/database.php
 return [
-    'host' => getenv('MYSQL_HOST') ?: 'localhost', // Menggunakan MYSQL_HOST
-    'port' => getenv('MYSQL_PORT') ?: '3306', // Menggunakan MYSQL_PORT
-    'dbname' => getenv('MYSQL_DATABASE') ?: 'mvc_db', // Menggunakan MYSQL_DATABASE
-    'username' => getenv('MYSQL_USER') ?: 'root', // Menggunakan MYSQL_USER
-    'password' => getenv('MYSQL_PASSWORD') ?: '', // Menggunakan MYSQL_PASSWORD
+    'host' => getenv('MYSQLHOST') ?: 'localhost', // Menggunakan MYSQL_HOST
+    'port' => getenv('MYSQLPORT') ?: '3306', // Menggunakan MYSQL_PORT
+    'dbname' => getenv('MYSQLDATABASE') ?: 'mvc_db', // Menggunakan MYSQL_DATABASE
+    'username' => getenv('MYSQLUSER') ?: 'root', // Menggunakan MYSQL_USER
+    'password' => getenv('MYSQLPASSWORD') ?: '', // Menggunakan MYSQL_PASSWORD
     'charset' => 'utf8mb4'
 ];
