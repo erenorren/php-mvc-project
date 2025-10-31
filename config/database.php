4c1ec36a4522eb1f4409bfc748f57ab97f2d18b7
erenorren <regishashrn07@gmail.com>
2025-11-01 06:18:20 +0700

    update

--- config/database.php
+++ config/database.php
@@ -1,13 +1,13 @@
-<!-- <?php
+<?php
 
-// return [
-    // 'host' => getenv('DB_HOST') ?: 'localhost',
-    // 'port' => getenv('DB_PORT') ?: '3306',
-    // 'dbname' => getenv('DB_NAME') ?: 'mvc_db',
-    // 'username' => getenv('DB_USER') ?: 'root',
-    // 'password' => getenv('DB_PASSWORD') ?: '',
-    // 'charset' => 'utf8mb4'
-// ]; -->
+return [
+    'host' => getenv('DB_HOST') ?: 'localhost',
+    'port' => getenv('DB_PORT') ?: '3306',
+    'dbname' => getenv('DB_NAME') ?: 'mvc_db',
+    'username' => getenv('DB_USER') ?: 'root',
+    'password' => getenv('DB_PASSWORD') ?: '',
+    'charset' => 'utf8mb4'
+];
 
 try {
     $host = getenv('MYSQLHOST');
