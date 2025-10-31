d5384cb8d63567c19b929a34a85069330f7aa043
erenorren <regishashrn07@gmail.com>
2025-11-01 06:28:11 +0700

    update

--- config/database.php
+++ config/database.php
@@ -1,10 +1,14 @@
 <?php
+// /config/database.php
 
 return [
-    'host' => getenv('DB_HOST') ?: 'localhost',
-    'port' => getenv('DB_PORT') ?: '3306',
-    'dbname' => getenv('DB_NAME') ?: 'mvc_db',
-    'username' => getenv('DB_USER') ?: 'root',
-    'password' => getenv('DB_PASSWORD') ?: '',
+    // --- GANTI DB_ dengan MYSQL_ ---
+    'host' => getenv('MYSQL_HOST') ?: 'localhost',
+    'port' => getenv('MYSQL_PORT') ?: '3306',
+    
+    // --- Tambahkan UNDERSCORE dan pastikan nama database yang benar ---
+    'dbname' => getenv('MYSQL_DATABASE') ?: 'mvc_db', // Pastikan nama DB di Railway sudah ditambahkan
+    'username' => getenv('MYSQL_USER') ?: 'root',
+    'password' => getenv('MYSQL_PASSWORD') ?: '',
     'charset' => 'utf8mb4'
 ];
